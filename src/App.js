0d23707a0342139ba543c08b66f13ae983cb0e86
Bhupender kumar <kumarbhupendra8171@gmail.com>
2023-07-04 13:36:33 +0530

    Build React-app using Create-react-app , then create class based component, then create NewsComponent and add in APP.js and Also create NewsITem component and add in the News Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+
+//Here in this project I use Class based component
+//And this app is for Showing daily news Via API Call
+//Here , I use the NewsDino.com for name my app
+export default class App extends Component {
+
+  
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+
+        <News />
+      </div>
+    )
+  }
 }
 
-export default App;
+//git add .
+//git commit -m "Initial commit"
+//git push origin main
