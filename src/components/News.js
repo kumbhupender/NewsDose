7e462a81ea9f7d566880f177eabcb75764781ad8
Bhupender kumar <kumarbhupendra8171@gmail.com>
2023-07-06 00:03:46 +0530

    Mapping through articles and render it

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,75 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+  articles = [
+      {
+          "source": {
+              "id": "news-com-au",
+              "name": "News.com.au"
+          },
+          "author": "Jai Bednall",
+          "title": "KP lays into England players in new twist",
+          "description": "Kevin Pietersen has unloaded on the England cricket team in the wake of the Second Test and the Jonny Bairstow controversy.",
+          "url": "https://www.news.com.au/sport/cricket/kevin-pietersen-squares-off-with-england-players-in-new-ashes-twist/news-story/39b3de9fba1278b268a2d9a57d9c9c02",
+          "urlToImage": "https://content.api.news/v3/images/bin/a58acaf0cd1435985f76a92887bdb82f",
+          "publishedAt": "2023-07-04T16:58:00Z",
+          "content": "Kevin Pietersen has unloaded on the England cricket team in the wake of the Second Test and the Jonny Bairstow controversy.\r\nPietersen hasn’t been shy about offering his opinion during the Ashes seri… [+3171 chars]"
+      },
+      {
+          "source": {
+              "id": "espn-cric-info",
+              "name": "ESPN Cric Info"
+          },
+          "author": null,
+          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+          "publishedAt": "2020-04-27T11:41:47Z",
+          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+      },
+      {
+          "source": {
+              "id": "espn-cric-info",
+              "name": "ESPN Cric Info"
+          },
+          "author": null,
+          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+          "publishedAt": "2020-03-30T15:26:05Z",
+          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+      },
+      {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        "publishedAt": "2020-03-30T15:26:05Z",
+        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+  ]
+  //using constructor
+  constructor(){
+    super();
+
+    console.log("Hello I am a constructor from news Comoponent");
+
+    this.state = {
+      articles : this.articles,
+      loading : false
+    }
+  }
+
+
+
   render() {
     return (
       <div className='container my-3'>
@@ -10,18 +79,19 @@ export class News extends Component {
         {/* importing the News component items 
           I want to 3 newsitem in one row
         */}
+        
+        
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="MyTitle" description="My Description" />
-          </div>
 
-          <div className="col-md-4">
-            <NewsItem title="MyTitle" description="My Description" />
-          </div>
+        {/* using map on articles */}
+          {this.state.articles.map((element) => {
+            console.log(element);
+            return <div className="col-md-4" key={element.url}>
+                    {/* Here i slice the words in limit and add ... for read more */}
+                    <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} url={element.urlToImage} newsId={element.url}/>
+                  </div>
+          })}
 
-          <div className="col-md-4">
-            <NewsItem title="MyTitle" description="My Description" />
-          </div>
         </div>
         
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
 
+  
   render() {
 
-    let {title , description} = this.props;
+    let {title , description, url, newsId} = this.props;
 
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={url} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsItem" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsId} className="btn btn-sm btn-primary" target='_blank'>Read More</a>
           </div>
         </div>
       </div>
