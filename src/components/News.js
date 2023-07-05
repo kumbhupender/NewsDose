3998192b5b78ff7cbf1e4729742f0b3945a11fc4
Bhupender kumar <kumarbhupendra8171@gmail.com>
2023-07-05 23:22:12 +0530

    Register on NewsAPi and Create a card using bootstrap

--- src/components/News.js
+++ src/components/News.js
@@ -4,11 +4,26 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is News Component.
+      <div className='container my-3'>
+        <h2>Top Headlines</h2>
 
-        {/* importing the News component items */}
-        <NewsItem />
+        {/* importing the News component items 
+          I want to 3 newsitem in one row
+        */}
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="My Description" />
+          </div>
+
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="My Description" />
+          </div>
+
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="My Description" />
+          </div>
+        </div>
+        
       </div>
     )
   }
